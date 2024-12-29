dfddcd2a1c89e02ecbc9f2f384f84f7903e35fa1
di-bon <dibon.francesco03@gmail.com>
2024-12-29 13:09:38 +0100

    Update src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ mod listener;
 mod server_logic;
 
 
-struct NullPointerDibServer {
+pub struct NullPointerDibServer {
     transmitter: Arc<Mutex<Transmitter>>,
     listener: Arc<Mutex<Listener>>,
 }
@@ -65,66 +65,15 @@ impl NullPointerDibServer {
             listener_clone.lock().unwrap().run()
         });
 
-        // let mut transmitter_clone = self.transmitter.clone();
-        // let transmitter_handle = thread::spawn(move || {
-        //     transmitter_clone.lock().unwrap().run()
-        // });
-
-        listener_handle.join();
-        // transmitter_handle.join();
-    }
-}
-
-/*
-pub fn run() {
-    // just for testing cargo clippy
-    // let (tx, rx) = unbounded::<Packet>();
-    // let (sl_tx, sl_rx) = unbounded::<Packet>();
-    // let transmitter = Transmitter::new(0, rx, tx, sl_rx, HashMap::new());
-    // transmitter.run();
-
-    pub fn new(
-        // the server's NodeId
-        node_id: NodeId,
-        // the channel the server listens on to receive packets from connected drones
-        listener_rx: Receiver<Packet>,
-        // the HashMap containing every connected drone
-        drones_tx: HashMap<NodeId, Sender<Packet>>,
-        // TODO: add simulation controller channel
-    ) {
-        let (internal_transmitter_to_listener_tx, internal_transmitter_to_listener_rx) = unbounded::<Packet>();
-        let (internal_listener_to_transmitter_tx, internal_listener_to_transmitter_rx) = unbounded::<Packet>();
-        let (internal_listener_to_server_logic_tx, internal_listener_to_server_logic_rx) = unbounded::<Packet>();
-        let (internal_server_logic_to_transmitter_tx, internal_server_logic_to_transmitter_rx) = unbounded::<Packet>();
-        let (listener_commands_tx, listener_commands_rx) = unbounded::<ListenerCommand>();
-
-        let transmitter = Transmitter::new(
-            node_id,
-            NodeType::Server,
-            internal_listener_to_transmitter_rx,
-            internal_transmitter_to_listener_tx,
-            internal_server_logic_to_transmitter_rx,
-            drones_tx
-        );
-        let listener = Listener::new(
-            node_id,
-            internal_listener_to_transmitter_tx,
-            internal_transmitter_to_listener_rx,
-            internal_listener_to_server_logic_tx,
-            listener_rx,
-            listener_commands_rx
-        );
-
+        let mut transmitter_clone = self.transmitter.clone();
         let transmitter_handle = thread::spawn(move || {
-            transmitter.run()
+            transmitter_clone.lock().unwrap().run()
         });
 
-        let listener_handle = thread::spawn(move || {
-            listener.run()
-        });
+        // TODO: listen for commands (only shutdown) (either from channel or from CLI)
+        // TODO: send shutdown command to listener and transmitter
 
         listener_handle.join();
+        transmitter_handle.join();
     }
 }
-
- */
