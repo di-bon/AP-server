dc4633705c2ef57711ebff9e5d451806b39b2697
di-bon <dibon.francesco03@gmail.com>
2025-02-04 00:14:18 +0100

    Add tests for content server

--- src/logic/content_server.rs
+++ src/logic/content_server.rs
@@ -153,7 +153,7 @@ impl ContentServer {
                 )
             }
             MediaRequest::Media(requested_media) => {
-                let filename = self.text_resources.iter().find(|res| *res == requested_media);
+                let filename = self.media_resources.iter().find(|res| *res == requested_media);
                 match filename {
                     Some(filename) => {
                         match self.read_file_as_bytes(filename) {
@@ -204,5 +204,362 @@ impl ContentServer {
         }
 
         Ok(files)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+    use crossbeam_channel::unbounded;
+    use messages::ChatRequest;
+    use super::*;
+
+    fn create_content_server(node_id: NodeId, resources_path: String) -> (
+        ContentServer,
+        Receiver<Message>,
+        Sender<Message>,
+        Sender<Command>,
+    ) {
+        let (logic_to_transmitter_tx, logic_to_transmitter_rx) = unbounded();
+        let (listener_to_logic_tx, listener_to_logic_rx) = unbounded();
+        let (command_tx, command_rx) = unbounded();
+
+        let server = ContentServer::new(
+            node_id,
+            logic_to_transmitter_tx,
+            listener_to_logic_rx,
+            command_rx,
+            resources_path,
+        );
+        (server, logic_to_transmitter_rx, listener_to_logic_tx, command_tx)
+    }
+
+    #[test]
+    fn initialize() {
+        let node_id = 0;
+        let resources_path = "./res".to_string();
+
+        let (server,
+            logic_to_transmitter_rx,
+            listener_to_logic_tx,
+            command_tx) = create_content_server(node_id, resources_path.clone());
+
+        assert_eq!(server.node_id, node_id);
+        let mut expected_text_resources = vec!["the quacking duck.txt".to_string(), "rust.txt".to_string()];
+        expected_text_resources.sort();
+        let mut server_text_resources = server.text_resources.clone();
+        server_text_resources.sort();
+        assert_eq!(server_text_resources, expected_text_resources);
+        let mut expected_media_resources: Vec<String> = vec!["ferris.png".to_string()];
+        expected_media_resources.sort();
+        let mut server_media_resources = server.media_resources.clone();
+        server_media_resources.sort();
+        assert_eq!(server_media_resources, expected_media_resources);
+        assert_eq!(server.resources_path, resources_path);
+    }
+
+    #[test]
+    fn check_text_request() {
+        let node_id = 0;
+        let resources_path = "./res".to_string();
+
+        let (server,
+            logic_to_transmitter_rx,
+            listener_to_logic_tx,
+            command_tx) = create_content_server(node_id, resources_path.clone());
+
+        let server = Arc::new(Mutex::new(server));
+        let server_clone = server.clone();
+
+        thread::spawn(move || {
+            server_clone.lock().unwrap().run();
+        });
+
+        let sender = 1;
+        let text_list_request = Message {
+            source: sender,
+            destination: node_id,
+            session_id: 0,
+            content: MessageType::Request(RequestType::TextRequest(TextRequest::TextList)),
+        };
+
+        let _ = listener_to_logic_tx.send(text_list_request);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+
+        let panic_message = "Wrong response";
+        let mut response_text_list = match response.content {
+            MessageType::Response(response) => {
+                match response {
+                    ResponseType::TextResponse(text_response) => {
+                        match text_response {
+                            TextResponse::TextList(list) => list,
+                            _ => panic!("{panic_message}"),
+                        }
+                    },
+                    _ => panic!("{panic_message}"),
+                }
+            }
+            _ => panic!("{panic_message}"),
+        };
+        response_text_list.sort();
+
+        let mut expected_text_list = vec!["the quacking duck.txt".to_string(), "rust.txt".to_string()];
+        expected_text_list.sort();
+
+        assert_eq!(response_text_list, expected_text_list);
+
+        let session_id = 10;
+        let text_request = Message {
+            source: sender,
+            destination: node_id,
+            session_id,
+            content: MessageType::Request(RequestType::TextRequest(TextRequest::Text("the quacking duck.txt".to_string()))),
+        };
+
+        let _ = listener_to_logic_tx.send(text_request);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+
+        let quacking_duck_content = fs::read_to_string("./res/the quacking duck.txt").unwrap();
+        let expected = Message {
+            source: node_id,
+            destination: sender,
+            session_id,
+            content: MessageType::Response(ResponseType::TextResponse(TextResponse::Text(quacking_duck_content))),
+        };
+
+        assert_eq!(response, expected);
+    }
+
+    #[test]
+    fn check_text_not_found() {
+        let node_id = 0;
+        let resources_path = "./res".to_string();
+
+        let (server,
+            logic_to_transmitter_rx,
+            listener_to_logic_tx,
+            command_tx) = create_content_server(node_id, resources_path.clone());
+
+        let server = Arc::new(Mutex::new(server));
+        let server_clone = server.clone();
+
+        thread::spawn(move || {
+            server_clone.lock().unwrap().run();
+        });
+
+        let sender = 1;
+
+        let session_id = 10;
+        let text_request = Message {
+            source: sender,
+            destination: node_id,
+            session_id,
+            content: MessageType::Request(RequestType::TextRequest(TextRequest::Text("boh.txt".to_string()))),
+        };
+
+        let _ = listener_to_logic_tx.send(text_request);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+
+        let expected = Message {
+            source: node_id,
+            destination: sender,
+            session_id,
+            content: MessageType::Response(ResponseType::TextResponse(TextResponse::NotFound("boh.txt".to_string()))),
+        };
+
+        assert_eq!(response, expected);
+    }
+
+    #[test]
+    fn check_media_request() {
+        let node_id = 0;
+        let resources_path = "./res".to_string();
+
+        let (server,
+            logic_to_transmitter_rx,
+            listener_to_logic_tx,
+            command_tx) = create_content_server(node_id, resources_path.clone());
+
+        let server = Arc::new(Mutex::new(server));
+        let server_clone = server.clone();
+
+        thread::spawn(move || {
+            server_clone.lock().unwrap().run();
+        });
+
+        let sender = 1;
+        let media_list_request = Message {
+            source: sender,
+            destination: node_id,
+            session_id: 0,
+            content: MessageType::Request(RequestType::MediaRequest(MediaRequest::MediaList)),
+        };
+
+        let _ = listener_to_logic_tx.send(media_list_request);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+
+        let panic_message = "Wrong response";
+        let mut response_media_list = match response.content {
+            MessageType::Response(response) => {
+                match response {
+                    ResponseType::MediaResponse(media_response) => {
+                        match media_response {
+                            MediaResponse::MediaList(list) => list,
+                            _ => panic!("{panic_message}"),
+                        }
+                    },
+                    _ => panic!("{panic_message}"),
+                }
+            }
+            _ => panic!("{panic_message}"),
+        };
+        response_media_list.sort();
+
+        let mut expected_media_list = vec!["ferris.png".to_string()];
+        expected_media_list.sort();
+
+        assert_eq!(response_media_list, expected_media_list);
+
+        let session_id = 10;
+        let media_request = Message {
+            source: sender,
+            destination: node_id,
+            session_id,
+            content: MessageType::Request(RequestType::MediaRequest(MediaRequest::Media("ferris.png".to_string()))),
+        };
+
+        let _ = listener_to_logic_tx.send(media_request);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+
+        let ferris_content = fs::read("./res/ferris.png").unwrap();
+        let expected = Message {
+            source: node_id,
+            destination: sender,
+            session_id,
+            content: MessageType::Response(ResponseType::MediaResponse(MediaResponse::Media(ferris_content))),
+        };
+
+        assert_eq!(response, expected);
+    }
+
+    #[test]
+    fn check_media_not_found() {
+        let node_id = 0;
+        let resources_path = "./res".to_string();
+
+        let (server,
+            logic_to_transmitter_rx,
+            listener_to_logic_tx,
+            command_tx) = create_content_server(node_id, resources_path.clone());
+
+        let server = Arc::new(Mutex::new(server));
+        let server_clone = server.clone();
+
+        thread::spawn(move || {
+            server_clone.lock().unwrap().run();
+        });
+
+        let sender = 1;
+
+        let session_id = 10;
+        let media_request = Message {
+            source: sender,
+            destination: node_id,
+            session_id,
+            content: MessageType::Request(RequestType::MediaRequest(MediaRequest::Media("boh.png".to_string()))),
+        };
+
+        let _ = listener_to_logic_tx.send(media_request);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+
+        let expected = Message {
+            source: node_id,
+            destination: sender,
+            session_id,
+            content: MessageType::Response(ResponseType::MediaResponse(MediaResponse::NotFound("boh.png".to_string()))),
+        };
+
+        assert_eq!(response, expected);
+    }
+
+    #[test]
+    fn check_unsupported_message() {
+        let node_id = 0;
+        let resources_path = "./res".to_string();
+
+        let (server,
+            logic_to_transmitter_rx,
+            listener_to_logic_tx,
+            command_tx) = create_content_server(node_id, resources_path.clone());
+
+        let server = Arc::new(Mutex::new(server));
+        let server_clone = server.clone();
+
+        thread::spawn(move || {
+            server_clone.lock().unwrap().run();
+        });
+
+        let sender = 1;
+        let unsupported_message = Message {
+            source: sender,
+            destination: node_id,
+            session_id: 0,
+            content: MessageType::Request(RequestType::ChatRequest(ChatRequest::ClientList)),
+        };
+
+        let _ = listener_to_logic_tx.send(unsupported_message);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+        let expected = Message {
+            source: node_id,
+            destination: sender,
+            session_id: 0,
+            content: MessageType::Error(ErrorType::Unsupported(RequestType::ChatRequest(ChatRequest::ClientList))),
+        };
+        assert_eq!(response, expected);
+    }
+
+    #[test]
+    fn check_discovery() {
+        let node_id = 0;
+        let resources_path = "./res".to_string();
+
+        let (server,
+            logic_to_transmitter_rx,
+            listener_to_logic_tx,
+            command_tx) = create_content_server(node_id, resources_path.clone());
+
+        let server = Arc::new(Mutex::new(server));
+        let server_clone = server.clone();
+
+        thread::spawn(move || {
+            server_clone.lock().unwrap().run();
+        });
+
+        let sender = 1;
+        let discovery_request = Message {
+            source: sender,
+            destination: node_id,
+            session_id: 0,
+            content: MessageType::Request(RequestType::DiscoveryRequest(())),
+        };
+
+        let _ = listener_to_logic_tx.send(discovery_request);
+
+        let response = logic_to_transmitter_rx.recv().unwrap();
+        let expected = Message {
+            source: node_id,
+            destination: sender,
+            session_id: 0,
+            content: MessageType::Response(ResponseType::DiscoveryResponse(ServerType::ContentServer)),
+        };
+        assert_eq!(response, expected);
     }
 }
